0c326cebd6b105a59dec4a1eca460f897b8c5d2a
KICKOFFMYBUTT <dhy_120@163.com>
2020-08-06 16:07:53 +0800

    finished input_data

--- Experiment1/phy1011/Elastic_Modulus.py
+++ Experiment1/phy1011/Elastic_Modulus.py
@@ -54,10 +54,55 @@ class Elastic_Modulus:
         pass
     
     def input_data(self, filename):
-        pass
+        ws = xlrd.open_workbook(filename).sheet_by_index(0)
+        # 从excel中读取数据
+        # 钢丝长度
+        L1 = float(ws.cell_value(3, 1))
+        L2 = float(ws.cell_value(3, 2))
+        L_ave = float(ws.cell_value(3, 3))
+        L_arr = [L1, L2]
+        # 镜子和尺的距离
+        H1 = float(ws.cell_value(5, 1))
+        H2 = float(ws.cell_value(5, 2))
+        H_ave = float(ws.cell_value(5, 3))
+        H_arr = [H1, H2]
+        # 光杠杆间距，重力加速度，螺旋测微计-零点误差
+        b = float(ws.cell_value(3, 6))
+        g = float(ws.cell_value(5, 6))
+        D0 = float(ws.cell_value(6, 4))
+        # 存储数据
+        self.data.update({"list_L":L_arr, "num_L":L_ave, "list_H":H_arr, "num_H":H_ave})
+        self.data.update({"num_b":b, "num_g":g, "num_D0":D0})
+        
+        # 读取D表格
+        D_raw_arr = []
+        D_arr = []
+        for cidx in range(1, 7):
+            D_raw_arr.append(float(ws.cell_value(9, cidx)))
+            D_arr.append(float(ws.cell_value(10, cidx)))
+        D_raw_ave = float(ws.cell_value(9, 7))
+        D_ave = float(ws.cell_value(10, 7))
+        # 存储
+        self.data.update({"list_D_raw":D_raw_arr, "list_D": D_arr})
+        self.data.update({"num_D_raw": D_raw_ave, "num_D": D_ave})
+        # 读取c表格
+        m_arr = []; r_p_arr = []; r_n_arr = []; r_ave_arr = []
+        for cidx in range(1, 9):
+            m_arr.append(float(ws.cell_value(14, cidx)))
+            r_p_arr.append(float(ws.cell_value(15, cidx)))
+            r_n_arr.append(float(ws.cell_value(16, cidx)))
+            r_ave_arr.append(float(ws.cell_value(17, cidx)))
+        # 存储
+        self.data.update({"list_m":m_arr, "list_r_p":r_p_arr, "list_r_n": r_n_arr, "list_r": r_ave_arr})
+        # 读取最后四个数据
+        self.data['num_m'] = float(ws.cell_value(19, 1))
+        self.uncertainty['D_L'] = float(ws.cell_value(19, 4))
+        self.uncertainty['D_H'] = float(ws.cell_value(19, 6))
+        self.uncertainty['D_b'] = float(ws.cell_value(19, 8))
+        # Finish!
 
     def calc_data(self):
-
+        
         pass
     
     def calc_uncertainty(self):
