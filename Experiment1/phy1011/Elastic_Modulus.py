3489fe0b9f0ccdecbd6d1a4a2a0d9452276e8c33
KICKOFFMYBUTT <dhy_120@163.com>
2020-08-06 17:46:51 +0800

    finished 1011

--- Experiment1/phy1011/Elastic_Modulus.py
+++ Experiment1/phy1011/Elastic_Modulus.py
@@ -1,6 +1,7 @@
 import xlrd
 import os, sys, shutil
-from numpy import sqrt, sys
+from numpy import sqrt
+
 
 sys.path.append("../..")
 from GeneralMethod.PyCalcLib import Fitting, Method
@@ -51,7 +52,7 @@ class Elastic_Modulus:
             print("实验报告生成完毕，正在打开......")
             Method.start_file(self.REPORT_OUTPUT_FILENAME)
             print("Done!")
-        pass
+        # Finished!
     
     def input_data(self, filename):
         ws = xlrd.open_workbook(filename).sheet_by_index(0)
@@ -102,16 +103,105 @@ class Elastic_Modulus:
         # Finish!
 
     def calc_data(self):
-        
-        pass
+        list_r = self.data['list_r']
+        list_dif_r, ave_c = Method.successive_diff(list_r)
+        self.data.update({"list_c": list_dif_r, "num_c": ave_c})
+        # 计算弹性模量E
+        m, g, L, H = self.data['num_m'], self.data['num_g'], self.data['num_L'], self.data['num_H']
+        D, b, ave_c = self.data['num_D'], self.data['num_b'], self.data['num_c']
+        PI = 3.1416
+        E = (16 * 4 * m * g * L * H) / (PI * (D**2) * b * ave_c)
+        E *= 1e6 # 单位换算
+        self.data['num_E'] = E
+        # Finish!
     
     def calc_uncertainty(self):
-        pass
+        # 几个只有b类的不确定度
+        u_L = self.uncertainty['u_L'] = self.uncertainty['D_L'] / sqrt(3)
+        u_H = self.uncertainty['u_H'] = self.uncertainty['D_H'] / sqrt(3)
+        u_b = self.uncertainty['u_b'] = self.uncertainty['D_b'] / sqrt(3)
+        L, H, D, b, c = self.data['num_L'], self.data['num_H'], self.data['num_D'], self.data['num_b'], self.data['num_c']
+
+        # D的不确定度
+        list_D = self.data['list_D']
+        ua_D = self.uncertainty['ua_D'] = Method.a_uncertainty(list_D)
+        ub_D = self.uncertainty['ub_D'] = 0.005 / sqrt(3)
+        u_D = self.uncertainty['u_D'] = sqrt(ua_D ** 2 + ub_D ** 2)
+        # c的不确定度
+        list_c = self.data['list_c']
+        ua_c = self.uncertainty['ua_c'] = Method.a_uncertainty(list_c)
+        ub_c = self.uncertainty['ub_c'] = 0.05 / sqrt(3)
+        u_c = self.uncertainty['u_c'] = sqrt(ua_c ** 2 + ub_c ** 2)
+        # E的不确定度合成
+        E = self.data['num_E']
+        u_E_E = self.uncertainty['u_E_E'] = sqrt((u_L/L)**2 + (u_H/H)**2 + 4 * (u_D/D)**2 + (u_b/b)**2 + (u_c/c)**2)
+        u_E = self.uncertainty['u_E'] = E * u_E_E
+        # 最终结果表述
+        self.data['final'] = Method.final_expression(E, u_E)
+        
 
     def fill_report(self):
-        pass
+        # 钢丝长度
+        self.report_data['L1'], self.report_data['L2'] = ["%.2f" % _i for _i in self.data['list_L']]
+        self.report_data['L_ave'] = "%.2f" % self.data['num_L']
+        # 平面镜距标尺的距离
+        self.report_data['H1'], self.report_data['H2'] = ["%.2f" % _i for _i in self.data['list_H']]
+        self.report_data['H_ave'] = "%.2f" % self.data['num_H']
+        # 光杠杆前后足间距
+        self.report_data['base_b'] = "%.2f" % self.data['num_b']
+        self.report_data['unc0_b'] = "%.2f" % self.uncertainty['D_b']
+        # 螺旋测微计的零点误差
+        self.report_data['D0'] = "%.3f" % self.data['num_D0']
+        # 钢丝直径 - 表格
+        for i, Draw_i in enumerate(self.data['list_D_raw'], start=1):
+            key = "Dc_%d" % i
+            self.report_data[key] = "%.3f" % Draw_i
+        self.report_data['Dc_ave'] = "%.3f" % self.data['num_D_raw']
+        for i, D_i in enumerate(self.data['list_D'], start=1):
+            key = "D_%d" % i
+            self.report_data[key] = "%.3f" % D_i
+        self.report_data['D_ave'] = "%.3f" % self.data['num_D']
+        # 加力 - 标尺读数
+        for i, m_i in enumerate(self.data['list_m'], start=1):
+            key = "m_%d" % i
+            self.report_data[key] = "%.3f" % m_i
+        for i, r_p_i in enumerate(self.data['list_r_p'], start=1):
+            key = "r_p%d" % i
+            self.report_data[key] = "%.2f" % r_p_i
+        for i, r_n_i in enumerate(self.data['list_r_n'], start=1):
+            key = "r_n%d" % i
+            self.report_data[key] = "%.2f" % r_n_i
+        for i, r_i in enumerate(self.data['list_r'], start=1):
+            key = "r_%d" % i
+            self.report_data[key] = "%.3f" % r_i
+        # 逐差法弹性模量
+        for i, c_i in enumerate(self.data['list_c'], start=1):
+            key = "c_%d" % i
+            self.report_data[key] = "%.3f" % c_i
+        self.report_data['c_ave'] = "%.3f" % self.data['num_c']
+        # 弹性模量计算结果
+        E_bse, E_pwr = Method.scientific_notation(self.data['num_E'])
+        self.report_data['E'] = "%.3fe%d" % (E_bse, E_pwr)
+        # 不确定度
+        self.report_data['u_L'] = "%.3f" % self.uncertainty['u_L']
+        self.report_data['u_H'] = "%.3f" % self.uncertainty['u_H']
+        self.report_data['u_b'] = "%.4f" % self.uncertainty['u_b']
+        # D的不确定度
+        self.report_data['ua_D'] = "%e" % self.uncertainty['ua_D']
+        self.report_data['ub_D'] = "%e" % self.uncertainty['ub_D']
+        self.report_data['u_D'] = "%e" % self.uncertainty['u_D']
+        # c的不确定度
+        self.report_data['ua_c'] = "%e" % self.uncertainty['ua_c']
+        self.report_data['ub_c'] = "%e" % self.uncertainty['ub_c']
+        self.report_data['u_c'] = "%e" % self.uncertainty['u_c']
+        # E的不确定度合成
+        self.report_data['u_E_E'], self.report_data['u_E'] = "%e" % self.uncertainty['u_E_E'], "%e" % self.uncertainty['u_E']
+        # 输出最终结果
+        self.report_data['final'] = self.data['final']
 
-    pass
+        RW = Report()
+        RW.load_replace_kw(self.report_data)
+        RW.fill_report(self.REPORT_TEMPLATE_FILENAME, self.REPORT_OUTPUT_FILENAME)
 
 if __name__ == '__main__':
     EM = Elastic_Modulus()
