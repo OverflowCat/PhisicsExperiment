e6627127df685488826755c71d6bb491a3a9ce22
WingDr <krq2000@126.com>
2020-09-04 00:17:59 +0800

    加入1011扭转法

--- Experiment1/phy1011/Elastic_Modulus.py
+++ Experiment1/phy1011/Elastic_Modulus.py
@@ -1,14 +1,15 @@
 import xlrd
 import os, sys, shutil
-from numpy import sqrt
+from numpy import sqrt, pi, abs
 
 
 sys.path.append("../..")
 from GeneralMethod.PyCalcLib import Fitting, Method
 from GeneralMethod.Report import Report
 
-class Elastic_Modulus:
-    def __init__(self, cwd=""): # 初始化实验类时给一个路径参数
+
+class ElasticModulus:
+    def __init__(self, cwd=""):  # 初始化实验类时给一个路径参数
 
         self.PREVIEW_FILENAME = cwd + "Preview.pdf"
         self.DATA_SHEET_FILENAME = cwd + "data.xlsx"
@@ -53,7 +54,7 @@ class Elastic_Modulus:
             Method.start_file(self.REPORT_OUTPUT_FILENAME)
             print("Done!")
         # Finished!
-    
+
     def input_data(self, filename):
         ws = xlrd.open_workbook(filename).sheet_by_index(0)
         # 从excel中读取数据
@@ -74,7 +75,7 @@ class Elastic_Modulus:
         # 存储数据
         self.data.update({"list_L":L_arr, "num_L":L_ave, "list_H":H_arr, "num_H":H_ave})
         self.data.update({"num_b":b, "num_g":g, "num_D0":D0})
-        
+
         # 读取D表格
         D_raw_arr = []
         D_arr = []
@@ -100,6 +101,41 @@ class Elastic_Modulus:
         self.uncertainty['D_L'] = float(ws.cell_value(19, 4))
         self.uncertainty['D_H'] = float(ws.cell_value(19, 6))
         self.uncertainty['D_b'] = float(ws.cell_value(19, 8))
+
+        # 读取下一个表格
+        ws = xlrd.open_workbook(filename).sheet_by_index(1)
+        # 从excel中读取数据
+        # 基础数据（数组，0：塑料圆柱，1：金属圆筒，2：圆球，3：金属杆，4：滑块）
+        basic_data = []
+        basic = {'m': float(ws.cell_value(3, 1)), 'd': float(ws.cell_value(4, 1))}
+        basic_data.append(basic)
+        basic = {'m': float(ws.cell_value(3, 2)), 'din': float(ws.cell_value(4, 3)),
+                 'dout': float(ws.cell_value(6, 3))}
+        basic_data.append(basic)
+        basic = {'m': float(ws.cell_value(3, 4)), 'd': float(ws.cell_value(4, 4))}
+        basic_data.append(basic)
+        basic = {'m': float(ws.cell_value(3, 5)), 'l': float(ws.cell_value(4, 5))}
+        basic_data.append(basic)
+        basic = {'m1': float(ws.cell_value(3, 6)), 'm2': float(ws.cell_value(3, 7)), 'din': float(ws.cell_value(4, 7)),
+                 'dout': float(ws.cell_value(5, 7)), 'h': float(ws.cell_value(6, 7))}
+        basic_data.append(basic)
+
+        # 每次计时周期数
+        times = float(ws.cell_value(8, 2))
+
+        # 摆动时间
+        time_meas = []
+        for row in range(5):
+            time_meas.append([float(i) for i in ws.row_values(row + 12, 3, 6)])
+
+        # 平行轴定理
+        time_meas_para = []
+        for row in range(5):
+            time_meas_para.append([float(i) for i in ws.row_values(row + 20, 3, 6)])
+
+        # 存储数据
+        self.data.update({"basic_data": basic_data, "times": times, "time_meas": time_meas,
+                          "time_meas_para": time_meas_para})
         # Finish!
 
     def calc_data(self):
@@ -109,12 +145,73 @@ class Elastic_Modulus:
         # 计算弹性模量E
         m, g, L, H = self.data['num_m'], self.data['num_g'], self.data['num_L'], self.data['num_H']
         D, b, ave_c = self.data['num_D'], self.data['num_b'], self.data['num_c']
-        PI = 3.1416
+        PI = pi
         E = (16 * 4 * m * g * L * H) / (PI * (D**2) * b * ave_c)
         E *= 1e6 # 单位换算
         self.data['num_E'] = E
+
+        # 计算转动惯量
+        basic_data = self.data['basic_data']
+        times = self.data['times']
+        time_meas = self.data['time_meas']
+        time_meas_para = self.data['time_meas_para']
+        # 计算真实周期
+        for i, row in enumerate(time_meas):
+            time_meas[i].append(Method.average(row))
+            time_meas[i].append(time_meas[i][-1] / times)
+        for i, row in enumerate(time_meas_para):
+            time_meas_para[i].append(Method.average(row))
+            time_meas_para[i].append(time_meas_para[i][-1] / times)
+
+        co = 4 * (pi ** 2)  # 提前计算一个小系数
+
+        # 计算K
+        I0 = 1 / 8 * basic_data[0]['m'] * (basic_data[0]['d'] ** 2) / 1e9
+        K = co * I0 / (time_meas[1][-1] ** 2 - time_meas[0][-1] ** 2)
+
+        co = K / co
+
+        # 金属圆筒
+        I1 = co * (time_meas[2][-1] ** 2 - time_meas[0][-1] ** 2)
+        I1_theo = 1 / 8 * basic_data[1]['m'] * (basic_data[1]['din'] ** 2 + basic_data[1]['dout'] ** 2) / 1e9
+        e1 = abs(I1 - I1_theo) / I1_theo * 100
+
+        # 圆球
+        I2 = co * (time_meas[3][-1] ** 2)
+        I2_theo = 1 / 10 * basic_data[2]['m'] * (basic_data[2]['d'] ** 2) / 1e9
+        e2 = abs(I2 - I2_theo) / I2_theo * 100
+
+        # 金属杆
+        I3 = co * (time_meas[4][-1] ** 2)
+        I3_theo = 1 / 12 * basic_data[3]['m'] * (basic_data[3]['l'] ** 2) / 1e9
+        e3 = abs(I3 - I3_theo) / I3_theo * 100
+
+        # 验证平行轴定理
+        x, y = [], []
+        for T in time_meas_para:
+            y.append(T[-1] ** 2)
+        for i in range(5):
+            x.append((i * 5 + 5) ** 2)
+        [k, b, r] = Fitting.linear(x, y, False)
+        k = k * 10000
+        J1 = 1 / 16 * basic_data[4]['m1'] * (basic_data[4]['din'] ** 2 + basic_data[4]['dout'] ** 2) \
+             + 1 / 12 * basic_data[4]['m1'] * (basic_data[4]['h'] ** 2)
+        J1 = J1 / 1e9
+        J2 = 1 / 16 * basic_data[4]['m2'] * (basic_data[4]['din'] ** 2 + basic_data[4]['dout'] ** 2) \
+             + 1 / 12 * basic_data[4]['m2'] * (basic_data[4]['h'] ** 2)
+        J2 = J2 / 1e9
+        k_theo = (basic_data[4]['m1'] + basic_data[4]['m2']) / co / 1000
+        b_theo = (J1 + J2 + I3) / co
+
+        # 更新数据
+        self.data.update({'time_meas': time_meas, 'time_meas_para': time_meas_para, 'I1': I0, 'K': K,
+                          'I2': I1, 'I2_theo': I1_theo, 'e2': e1,
+                          'I3': I2, 'I3_theo': I2_theo, 'e3': e2,
+                          'I4': I3, 'I4_theo': I3_theo, 'e4': e3,
+                          '2k': k, '2k_theo': k_theo, '2b': b, '2b_theo': b_theo, '2r': r,
+                          'J1': J1, 'J2': J2})
         # Finish!
-    
+
     def calc_uncertainty(self):
         # 几个只有b类的不确定度
         u_L = self.uncertainty['u_L'] = self.uncertainty['D_L'] / sqrt(3)
@@ -138,7 +235,6 @@ class Elastic_Modulus:
         u_E = self.uncertainty['u_E'] = E * u_E_E
         # 最终结果表述
         self.data['final'] = Method.final_expression(E, u_E)
-        
 
     def fill_report(self):
         # 钢丝长度
@@ -199,10 +295,47 @@ class Elastic_Modulus:
         # 输出最终结果
         self.report_data['final'] = self.data['final']
 
+        # 扭转法
+
+        # 基础数据
+        for i, basic in enumerate(self.data['basic_data']):
+            for key in basic.keys():
+                self.report_data['s21' + key + str(i)] = "%.2f" % basic[key]
+        # 每次测量周期数
+        self.report_data['times'] = self.data['times']
+        # 测量周期
+        for i, T in enumerate(self.data['time_meas']):
+            for j, detail in enumerate(T):
+                self.report_data['s212' + str(i) + str(j)] = "%.2f" % detail
+        # 平行轴定理
+        for i, T in enumerate(self.data['time_meas_para']):
+            for j, detail in enumerate(T):
+                self.report_data['s213' + str(i) + str(j)] = "%.2f" % detail
+        # 其它计算值
+        self.report_data['I1'] = "%.4e" % self.data['I1']
+        self.report_data['I2'] = "%.4e" % self.data['I2']
+        self.report_data['I3'] = "%.4e" % self.data['I3']
+        self.report_data['I4'] = "%.4e" % self.data['I4']
+        self.report_data['I2_theo'] = "%.4e" % self.data['I2_theo']
+        self.report_data['I3_theo'] = "%.4e" % self.data['I3_theo']
+        self.report_data['I4_theo'] = "%.4e" % self.data['I4_theo']
+        self.report_data['e2'] = "%.2f" % self.data['e2']
+        self.report_data['e3'] = "%.2f" % self.data['e3']
+        self.report_data['e4'] = "%.2f" % self.data['e4']
+        self.report_data['K'] = "%.2e" % self.data['K']
+        self.report_data['2k'] = "%d" % self.data['2k']
+        self.report_data['2b'] = "%.4f" % self.data['2b']
+        self.report_data['2r'] = "%.4f" % self.data['2r']
+        self.report_data['2k_theo'] = "%d" % self.data['2k_theo']
+        self.report_data['2b_theo'] = "%.4f" % self.data['2b_theo']
+        self.report_data['J1'] = "%.4e" % self.data['J1']
+        self.report_data['J2'] = "%.4e" % self.data['J2']
+
+
         RW = Report()
         RW.load_replace_kw(self.report_data)
         RW.fill_report(self.REPORT_TEMPLATE_FILENAME, self.REPORT_OUTPUT_FILENAME)
 
+
 if __name__ == '__main__':
-    EM = Elastic_Modulus()
-    pass
+    EM = ElasticModulus()
